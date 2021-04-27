b5d718d39bb77637f6f708e5471e2f460287a7e9
wfgv <willian.voelz@gmail.com>
2021-04-27 03:11:56 +0000

    multer intro

---
+++ models/image.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var imageSchema = new mongoose.Schema({
+    filename: String,
+    originalName: String,
+    desc: String,
+
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model('Image', ImageSchema);
